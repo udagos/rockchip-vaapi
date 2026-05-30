e194b9c425978a3e694713e9131ac4590c833a08
udago <udagos44@gmail.com>
2026-05-30 18:52:21 +0800

    Fix VAProfileH264High10 undeclared error robustly

--- src/rockchip_drv_video.c
+++ src/rockchip_drv_video.c
@@ -165,7 +165,7 @@ static MppCodingType profile_to_coding(VAProfile p) {
     case VAProfileH264ConstrainedBaseline:
     case VAProfileH264Main:
     case VAProfileH264High:
-#if defined(VA_CHECK_VERSION) && VA_CHECK_VERSION(1, 13, 0)
+#ifdef VAProfileH264High10
     case VAProfileH264High10:
 #endif
         return MPP_VIDEO_CodingAVC;
@@ -185,7 +185,7 @@ static int profile_idc(VAProfile p) {
     case VAProfileH264ConstrainedBaseline: return 66;
     case VAProfileH264Main:                return 77;
     case VAProfileH264High:                return 100;
-#if defined(VA_CHECK_VERSION) && VA_CHECK_VERSION(1, 13, 0)
+#ifdef VAProfileH264High10
     case VAProfileH264High10:              return 110;
 #endif
     default:                               return 100;
@@ -228,7 +228,7 @@ static VAStatus rk_QueryConfigProfiles(VADriverContextP ctx,
     list[i++] = VAProfileH264ConstrainedBaseline;
     list[i++] = VAProfileH264Main;
     list[i++] = VAProfileH264High;
-#if defined(VA_CHECK_VERSION) && VA_CHECK_VERSION(1, 13, 0)
+#ifdef VAProfileH264High10
     list[i++] = VAProfileH264High10;
 #endif
     list[i++] = VAProfileHEVCMain;
