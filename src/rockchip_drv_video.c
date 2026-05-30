6868fe8e0a0b7278f409a316040401f4e87d8ca4
udago <udagos44@gmail.com>
2026-05-30 19:06:50 +0800

    Restore VA_CHECK_VERSION logic

--- src/rockchip_drv_video.c
+++ src/rockchip_drv_video.c
@@ -180,7 +180,7 @@ static MppCodingType profile_to_coding(VAProfile p) {
     case VAProfileH264ConstrainedBaseline:
     case VAProfileH264Main:
     case VAProfileH264High:
-#if defined(VA_MAJOR_VERSION) && (VA_MAJOR_VERSION > 1 || (VA_MAJOR_VERSION == 1 && VA_MINOR_VERSION >= 13))
+#if VA_CHECK_VERSION(1, 13, 0)
     case VAProfileH264High10:
 #endif
         return MPP_VIDEO_CodingAVC;
@@ -200,7 +200,7 @@ static int profile_idc(VAProfile p) {
     case VAProfileH264ConstrainedBaseline: return 66;
     case VAProfileH264Main:                return 77;
     case VAProfileH264High:                return 100;
-#if defined(VA_MAJOR_VERSION) && (VA_MAJOR_VERSION > 1 || (VA_MAJOR_VERSION == 1 && VA_MINOR_VERSION >= 13))
+#if VA_CHECK_VERSION(1, 13, 0)
     case VAProfileH264High10:              return 110;
 #endif
     default:                               return 100;
@@ -243,7 +243,7 @@ static VAStatus rk_QueryConfigProfiles(VADriverContextP ctx,
     list[i++] = VAProfileH264ConstrainedBaseline;
     list[i++] = VAProfileH264Main;
     list[i++] = VAProfileH264High;
-#if defined(VA_MAJOR_VERSION) && (VA_MAJOR_VERSION > 1 || (VA_MAJOR_VERSION == 1 && VA_MINOR_VERSION >= 13))
+#if VA_CHECK_VERSION(1, 13, 0)
     list[i++] = VAProfileH264High10;
 #endif
     list[i++] = VAProfileHEVCMain;
